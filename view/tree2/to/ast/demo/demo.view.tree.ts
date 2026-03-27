c5b0e05cdee16b03fed8d80f231c03bd49ebe57b
Vladislav Atakhanov <eloh1mzee@gmail.com>
2026-03-28 01:23:06 +0500

    feat: add demo

--- view/tree2/to/ast/demo/demo.view.tree.ts
+++ view/tree2/to/ast/demo/demo.view.tree.ts
@@ -6,5 +6,12 @@ namespace $.$$ {
 			const ast = this.$.$mol_view_tree2_to_ast(tree)
 			return this.$.$mol_tree2_to_string(ast)
 		}
+		@$mol_mem
+		js() {
+			const tree = this.$.$mol_tree2_from_string(this.src())
+			const text = this.$.$mol_view_tree2_to_text(tree)
+			const str = this.$.$mol_tree2_text_to_string(text)
+			return str
+		}
 	}
 }
