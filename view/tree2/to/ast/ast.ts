e7b368273db239b8930bb5c726c1e1a1128e82db
Vladislav Atakhanov <eloh1mzee@gmail.com>
2026-03-29 11:38:43 +0500

    refactor: add Ast type

--- view/tree2/to/ast/ast.ts
+++ view/tree2/to/ast/ast.ts
@@ -1,11 +1,22 @@
 namespace $ {
 	const err = $mol_view_tree2_error_str
 
-	type Type = (typeof types)[number]
-	const Type = (tree: $mol_tree2, type: Type) => [tree.struct('type', [tree.data(type)])]
-	const Literal = (tree: $mol_tree2) => [...Type(tree, 'literal'), tree.struct(`raw`, [tree])]
+	const Type = (tree: $mol_tree2, type: Property['type']) => [tree.struct('type', [tree.data(type)])]
+	const Literal = (tree: $mol_tree2) => [...Type(tree, types.literal), tree.struct(`raw`, [tree])]
+	const Null = (tree: $mol_tree2) => [
+		...Type(tree, types.literal),
+		tree.struct(`raw`, [tree]),
+		...(tree.kids.length > 0
+			? [
+					tree.struct(
+						`hint`,
+						tree.kids.map(k => k.data(k.type)),
+					),
+				]
+			: []),
+	]
 	const Class = (tree: $mol_tree2, parent: $mol_tree2, props: $mol_tree2[]) => [
-		...Type(tree, 'class'),
+		...Type(tree, types.class),
 		tree.struct('extends', [parent.data(parent.type)]),
 		tree.struct('properties', [object(parent, props)]),
 	]
@@ -14,7 +25,6 @@ namespace $ {
 		return prop.struct(name, [
 			object(prop, [
 				...other,
-
 				...(key
 					? [prop.struct('key', [key.length === 1 ? prop.struct('true', []) : prop.data(key.slice(1))])]
 					: []),
@@ -29,9 +39,8 @@ namespace $ {
 
 	const array = (root: $mol_tree2, items: readonly $mol_tree2[]) => root.struct('/', items)
 	const object = (root: $mol_tree2, items: readonly $mol_tree2[]) => root.struct('*', items)
-	const types = ['literal', 'class', 'pull', 'put', 'bidi', 'dictionary', 'list', 'i18n'] as const
 
-	function Arrow(this: $, v: $mol_tree2, type: 'bidi' | 'put') {
+	function Arrow(this: $, v: $mol_tree2, type: (typeof types)['bidi'] | (typeof types)['put']) {
 		return [...Type(v, type), v.struct('property', [v.data(name_of.call(this, v.kids[0]))])]
 	}
 
@@ -50,7 +59,7 @@ namespace $ {
 						props.map(prop => {
 							const name = name_of.call(this, prop)
 							const keywords = Object.fromEntries(
-								['null', 'false', 'true', 'Infinity', '-Infinity', 'NaN'].map(k => [k, k]),
+								['false', 'true', 'Infinity', '-Infinity', 'NaN'].map(k => [k, k]),
 							)
 							const extend = (tree: $mol_tree2) => {
 								if (tree.type === '^') {
@@ -65,6 +74,7 @@ namespace $ {
 									...Object.fromEntries(
 										Object.entries(keywords).map(([k, l]) => [k, t => Literal(t)]),
 									),
+									null: Null,
 									'=': v => {
 										const path = []
 										let kid = v.kids[0]
@@ -77,7 +87,7 @@ namespace $ {
 									'<=': v => Arrow.call(this, v, 'put'),
 									'<=>': v => Arrow.call(this, v, 'bidi'),
 									'@': (v, b, { chain }) => [
-										...Type(v, 'i18n'),
+										...Type(v, types.i18n),
 										v.struct(`raw`, v.kids),
 										v.struct('id', [
 											v.data(`${klass.type}_${name}${chain.length ? `_${chain}` : ''}`),
@@ -86,7 +96,7 @@ namespace $ {
 									'': (input, belt, context) => {
 										if (input.type[0] === '*') {
 											return [
-												...Type(input, 'dictionary'),
+												...Type(input, types.dictionary),
 												input.struct('properties', [
 													array(
 														input,
@@ -112,13 +122,13 @@ namespace $ {
 													),
 												]),
 												...(input.type.length > 0
-													? [input.struct('type', [input.data(input.type.substring(1))])]
+													? [input.struct('hint', [input.data(input.type.substring(1))])]
 													: []),
 											]
 										}
 										if (input.type[0] === '/') {
 											return [
-												...Type(input, 'list'),
+												...Type(input, types.list),
 												input.struct('items', [
 													array(
 														input,
@@ -133,7 +143,7 @@ namespace $ {
 													),
 												]),
 												...(input.type.length > 0
-													? [input.struct('type', [input.data(input.type.substring(1))])]
+													? [input.struct('hint', [input.data(input.type.substring(1))])]
 													: []),
 											]
 										}
@@ -173,4 +183,30 @@ namespace $ {
 
 		return object(descr, classes)
 	}
+
+	type PropertyName = string & { __brand: 'PropertyName' }
+	type _Ast = {
+		class: {
+			extends: string
+			properties: Record<PropertyName, Property & { key: boolean; writable: boolean }>
+		}
+		pull: { path: PropertyName[] }
+		put: { property: PropertyName }
+		bidi: { property: PropertyName }
+		literal: { raw: string | number | boolean } | { raw: null; hint?: string }
+		i18n: { raw: string; id: string }
+		dictionary: { properties: Array<{ self: true } | PropertyName | [string, Property]> }
+		list: { properties: Array<{ self: true } | PropertyName | Property> }
+	}
+	type Ast = { [K in keyof _Ast]: { type: K } & _Ast[K] }
+	export type $mol_view_tree2_ast_types = Ast
+	type Property = Ast[keyof Ast]
+	const types = Object.fromEntries(
+		(['class', 'pull', 'put', 'bidi', 'literal', 'i18n', 'dictionary', 'list'] satisfies (keyof Ast)[]).map(t => [
+			t,
+			t,
+		]),
+	) as { [k in keyof Ast]: k }
+
+	export type $mol_view_tree2_ast = Record<string, Ast['class']>
 }
