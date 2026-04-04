7eb403ff7cf2b2942265bfb34feb39866c4049fb
Vladislav Atakhanov <eloh1mzee@gmail.com>
2026-04-04 21:15:59 +0500

    feat: ast_to_js init

--- view/tree2/to/ast/demo/demo.view.tree.ts
+++ view/tree2/to/ast/demo/demo.view.tree.ts
@@ -29,12 +29,65 @@ namespace $.$$ {
 		@$mol_mem
 		raw_js() {
 			const tree = this.$.$mol_tree2_from_string(this.value())
-			const js = this.$.$mol_view_tree2_to_text(tree)
-			return this.$.$mol_tree2_text_to_string(js)
+			const js = this.$.$mol_view_tree2_to_js(tree)
+			// return this.$.$mol_tree2_to_string(js)
+			const text = this.$.$mol_tree2_js_to_text(js)
+			return this.$.$mol_tree2_text_to_string(text)
 		}
 		@$mol_mem
 		js() {
-			return this.$.$mol_tree2_to_string(this.tree())
+			const js = ast_to_js.call(this.$, this.ast())
+			// return this.$.$mol_tree2_to_string(js)
+			return this.$.$mol_tree2_text_to_string(this.$.$mol_tree2_js_to_text(js))
 		}
+	}
+
+	function ast_to_js(this: $, ast: $mol_view_tree2_to_ast) {
+		const addons = [] as $mol_tree2[]
+		return ast.list([
+			ast.struct(';', [
+				...ast.classes.map(klass => {
+					const parent = klass.extends_
+					return klass.struct('=', [
+						klass.struct('()', [klass.struct('$'), klass.struct('[]', [klass.data(klass.type)])]),
+						klass.struct('class', [
+							klass.struct(klass.type),
+							parent.struct('extends', [
+								parent.struct('()', [
+									parent.struct('$'),
+									parent.struct('[]', [parent.data(parent.type)]),
+								]),
+							]),
+							klass.struct(
+								'{}',
+								klass.properties.map((prop, name) => {
+									if (prop.key.val || prop.writable.val) {
+										addons.push(
+											name.struct('()', [
+												prop.key.struct(prop.key.val ? '$mol_mem_key' : '$mol_mem'),
+												name.struct('(,)', [
+													name.struct('()', [
+														klass.struct('$'),
+														name.struct('[]', [klass.data(klass.type)]),
+														name.struct('[]', [name.data('prototype')]),
+													]),
+													name.data(name.value),
+												]),
+											]),
+										)
+									}
+									return name.struct('.', [
+										name.data(name.value),
+										name.struct('(,)'),
+										name.struct('{;}', [name.struct('return')]),
+									])
+								}),
+							),
+						]),
+					])
+				}),
+				...addons,
+			]),
+		])
 	}
 }
