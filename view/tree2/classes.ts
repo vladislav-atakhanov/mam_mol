f09e5857385b71006402f99d73a2a79f4f48223f
Vladislav Atakhanov <eloh1mzee@gmail.com>
2026-04-29 12:07:10 +0500

    refactor: $mol_fail if invalid syntax

--- view/tree2/classes.ts
+++ view/tree2/classes.ts
@@ -1,26 +1,29 @@
 namespace $ {
-	const to_writable = (i: $mol_tree2, kids?: readonly $mol_tree2[]) =>
-		i.type.includes('?') ? i.clone(kids ?? i.kids) : i.struct(i.type + '?', kids ?? i.kids)
-
-	const bidi = (left: $mol_tree2, right: $mol_tree2) =>
-		to_writable(left, [(left.kids[0] ?? left).struct('<=>', [to_writable(right)])])
-
+	const err = $mol_view_tree2_error_str
+	const is_writable = (input: $mol_tree2) => input.type.includes('?')
+	function ensure_writable(input?: $mol_tree2) {
+		if (input && !is_writable(input)) return $mol_fail(err`Expected writable at ${input.span}`)
+	}
+	function ensure_readonly(input?: $mol_tree2) {
+		if (input && is_writable(input)) return $mol_fail(err`Expected readonly at ${input.span}`)
+	}
 	export function $mol_view_tree2_classes(defs: $mol_tree2) {
 		return defs.clone(
 			defs.hack({
 				'-': () => [],
-				'': (input, belt, context) => {
+				'': (input, belt) => {
 					const kid = input.kids[0]
 					switch (kid?.type) {
 						case '<=>':
-							return [bidi(input, kid.hack(belt)[0])]
-						case '=>': {
-							const index = input.type.indexOf('?')
-							if (index !== -1) {
-								if (kid.kids[0]?.type?.includes('?')) return [bidi(input, kid.hack(belt)[0])]
-								else return [input.struct(input.type.substring(0, index), input.hack(belt))]
-							}
-						}
+							ensure_writable(input)
+							ensure_writable(kid.kids[0])
+							break
+						case '=>':
+							;(is_writable(input) ? ensure_writable : ensure_readonly)(kid.kids[0])
+							break
+						case '<=':
+							ensure_readonly(input)
+							break
 					}
 					return [input.clone(input.hack(belt))]
 				},
