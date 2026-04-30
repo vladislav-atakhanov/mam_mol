9c0a7d0ff954f91813a6ce9a61b961488be56477
Stefan Zerkalica <zerkalica@gmail.com>
2026-04-30 18:06:09 +0300

    Revert "debug logs more"
    This reverts commit 7e3f59f8ca0aeca7a8a534d9cdb9c3c9a2d1f8d2.

--- build/build.node.ts
+++ build/build.node.ts
@@ -732,7 +732,6 @@ namespace $ {
 		@ $mol_mem_key
 		bundle( [ path , bundle = '' ] : [ path : string , bundle? : string ] ) {
 			
-			console.log('!!! bundle 1', path, bundle)
 			bundle = bundle && bundle.replace( /\.map$/ , '' )
 			
 			var envsDef = [ 'web' , 'node' ]
@@ -753,7 +752,6 @@ namespace $ {
 			
 			var res : $mol_file[] = []
 			
-			console.log('!!! bundle 2', path, bundle)
 			envs.forEach(
 				env => {
 					var exclude = envsDef.filter( e => e !== env ).concat( stages )
@@ -825,7 +823,6 @@ namespace $ {
 			if( !bundle || /\//.test( bundle ) ) {
 				res = res.concat( this.bundleFiles( [ path , [ 'node' ] ] ) )
 			}
-			console.log('!!! bundle 3', path, bundle)
 			
 			return res
 		}
